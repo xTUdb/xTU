8380ff4e41be889c9d64d3d0f9ab5f9062333723
Konthon <konthon@hotmail.co.th>
2018-10-25 22:31:31 +0700

    foot

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,16 @@
     <!-- Footer -->
     <footer class="container">
+        <div class="column1">
+            <p>xTU LOGO </p>
+            <p> <a href="#">About Us</a></p>
+            <p><a href="#">Contact Us</a></p>
+            </div>
+        <div class="column2">
+            <p><a href="#">Report abuse/place</a></p>
+        </div>
+        <div class="column3">
+            <p>Design by ProjectX Group</p>
         <br>
-        Design by ProjectX Group <br>
         Contact us
         <!-- FACEBOOK -->
         <a href="https://www.facebook.com/"><i class="fab fa-facebook-square fa-2x"> </i></a>
@@ -9,7 +18,7 @@
         <a href="#"><i class="fab fa-line fa-2x"> </i></a>
         <!-- TWITTER -->
         <a href="https://www.twitter.com/"><i class="fab fa-twitter-square fa-2x"> </i></a>
-
+        </div>
     </footer>
     <!-- Footer End -->
 
