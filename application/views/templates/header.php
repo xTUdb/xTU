93ea261fbc8405143a254910f7f105b77d0ba865
x <x@xs-MacBook-Pro.local>
2018-10-25 23:39:29 +0700

    navbar fix

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>xTU</title>
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bulma.css" />
+  <!-- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bulma.css" /> -->
   <!-- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css" /> -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
