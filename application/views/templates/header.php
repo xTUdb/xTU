1f749d57cb3db9e61e2c56ab38da311665cc9519
x <x@xs-MacBook-Pro.local>
2018-10-25 22:33:32 +0700

    carousel

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,10 +5,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>xTU</title>
-  <!-- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bulma.css" /> -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bulma.css" />
   <!-- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css" /> -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+  <script type="text/javascript" src="/node_modules/bulma-extensions/bulma-carousel/dist/bulma-carousel.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-3.3.1.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/slick.js"></script>
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/slick.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/slick-theme.css">
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> -->
 </head>
 <body>
     <!-- Navbar -->
