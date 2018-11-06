75fc493eda3fdecb3f8229bca019cf1935407433
Konthon <konthon@hotmail.co.th>
2018-11-06 14:20:23 +0700

    search_update2

--- application/views/search.php
+++ application/views/search.php
@@ -8,5 +8,17 @@
 </div>
 <div class="container">
     <div class="query-head">Recent Search</div>
+
+    <!-- item -->
+    <div class="query-card">
+        <div class="query-img">
+        <img src="https://via.placeholder.com/300x175" alt="">
+        <div class="query-category"></div>
+            <img class="query-category-img" src="https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Food-Dome-512.png" alt="food">Food
+        </div>
+        <span class="query-name-place">Secret Hideout</span>
+        <p>Description Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, quo. Rem quo maxime hic sint, aliquid, eaque sed assumenda, beatae officia mollitia laboriosam corrupti porro recusandae sequi quibusdam soluta nesciunt. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae accusamus numquam recusandae velit mollitia cumque repudiandae et, veritatis temporibus alias reiciendis saepe incidunt sed aliquid quos nulla ducimus commodi quibusdam.</p>
+    </div><!-- End-item -->
     <div class="query-head">Most View</div>
+</div>​
 </div>
