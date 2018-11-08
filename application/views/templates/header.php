982965c2e5d27cd072d0effac0523233c4c10359
Konthon <konthon@hotmail.co.th>
2018-11-09 00:18:57 +0700

    navbar change v2.2

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,25 +20,52 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
     <!-- Navbar -->
-    <div id="header">
+    <div class="navbar-box">
         <div class="container">
-            <a href="<?php echo base_url(); ?>"><div id="logo"><?= "<img src='https://dl.dropbox.com/s/tupetot53ck72aw/logo_xTU.png' alt='xTU' width='60px'>" ?></div></a>
-            <ul>
-                <li><a style="opacity: 0; background-color: white;" href="#">.........</a></li>
-                <li class="dropdown">
-                    <a href="javascript:void(0)" class="dropbtn">Categories <i class="fas fa-angle-down"></i></a>
-                        <div class="dropdown-content">
-                        <a href="#">Food</a>
-                        <a href="#">Café</a>
-                        <a href="#">Store</a>
-                        <a href="#">Pharmacy</a>
-                        <a href="#">Others</a>
-                        </div>
-                </li>
-                <li style="float: right;"><a href="<?php echo base_url(); ?>search"><i class="fas fa-search"></i></a></li>
-                <li style="float: right;"><a href="<?php echo base_url(); ?>login">Login</a></li>
-                <li style="float: right;"><a href="#login">ThammaTrans</a></li>
-            </ul>
-        </div>
+            <div class="nav-left">
+                <img id="title-logo" src="https://i.imgur.com/mZfzt9s.png" alt="1024x545">
+                <div class="nav-list dd-h">Categories <i class="fas fa-angle-down"></i>
+            
+            <!-- Dropdown container -->
+            <div class="dropdown-box">
+                <div class="dropdown-subcat">
+                    <div class="dropdown-subcat-head"><i class="fas fa-utensils"></i> Food & Drink</div>
+                    <div class="dropdown-item">
+                        <a href=""><img src="https://cdn.icon-icons.com/icons2/881/PNG/512/Hamburger_icon-icons.com_68741.png" alt=""><div class="dd-t">Food</div></a>
+                        <a href=""><img src="https://marketplace.canva.com/MACaIUDtCx8/1/thumbnail_large/canva-cafe-icon-concept-MACaIUDtCx8.png" alt=""><div class="dd-t">Café</div></a>
+                        <a href=""><img src="https://cdn.icon-icons.com/icons2/881/PNG/512/Cocktail_icon-icons.com_68766.png" alt=""><div class="dd-t">Drink</div></a>
+                        <a href=""><img src="https://image.flaticon.com/icons/png/512/138/138307.png" alt=""><div class="dd-t">Stall</div></a>
+                    </div>
+                </div><!-- END Subcat -->
+                <div class="dropdown-subcat">
+                    <div class="dropdown-subcat-head"><i class="fas fa-store"></i> Stores</div>
+                    <div class="dropdown-item">
+                        <a href=""><img src="https://pbs.twimg.com/profile_images/955329141707915265/FzsHnjOp_400x400.jpg" alt=""><div class="dd-t">7-11</div></a>
+                        <a href=""><img src="https://i1.wp.com/muchikoro.com/wordpress/wp-content/uploads/2016/07/FamilyMart-300x300.png" alt=""><div class="dd-t">Family Mart</div></a>
+                        <a href=""><img src="https://superryu247.files.wordpress.com/2013/04/lawson_logo.jpg" alt=""><div class="dd-t">Lawson 108</div></a>
+                        <a href=""><img src="https://www.oikosfamily.co.za/wp-content/uploads/2016/03/store-Icon-1.png" alt=""><div class="dd-t">Others</div></a>
+                    </div>
+                </div><!-- END Subcat -->
+                <div class="dropdown-subcat">
+                    <div class="dropdown-subcat-head"><i class="fas fa-user-cog"></i> Services</div>
+                    <div class="dropdown-item">
+                        <a href=""><img src="https://pbs.twimg.com/profile_images/955329141707915265/FzsHnjOp_400x400.jpg" alt=""><div class="dd-t">7-11</div></a>
+                        <a href=""><img src="https://i1.wp.com/muchikoro.com/wordpress/wp-content/uploads/2016/07/FamilyMart-300x300.png" alt=""><div class="dd-t">Family Mart</div></a>
+                        <a href=""><img src="https://superryu247.files.wordpress.com/2013/04/lawson_logo.jpg" alt=""><div class="dd-t">Lawson 108</div></a>
+                    </div>
+                </div><!-- END Subcat -->
+            </div><!-- END dropdown container --></div><!-- END dropdown menu -->
+            </div><!-- END Nav-left -->
+
+            <div class="nav-right">
+                <span class="nav-list">ThammaTrans</span>
+                <span class="nav-item nav-user"><i class="fas fa-user-circle nav-icon"></i>
+                    <span class="nav-hide-text">&nbsp; Login / Sign up</span>
+                </span>
+                <span class="nav-item"><i class="fas fa-search fa-lg"></i>
+                    <span class="nav-hide-text">&nbsp; Search</span>
+                </span>
+            </div><!-- END Nav-right -->
+        </div><!-- END container -->
     </div>
-    <!-- Navbar End -->
+    <!-- END NAVBAR BOX -->
